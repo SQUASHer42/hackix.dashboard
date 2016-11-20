3ccac6c20e6646001655ff87b531035b6d8723f5
SQUASHer42 <squasher42@gmail.com>
2016-11-19 19:54:42 -0500

    working add dropdowns

--- IX Analytical Dashboard/js/add.js
+++ IX Analytical Dashboard/js/add.js
@@ -1,10 +1,30 @@
+
 $(document).ready(function(){
-  $(".dropbtn").hover(function(){
-    $(this).css("background-color", "#3e8e41");
+  $(".choicebtn").on("click", function(){
+    var text = $(this).text();
+    $("#charts").text(text);
   });
 
-  $(".dropbtn").click(function(){
-    $(".dropdown-content p").css("display", "none");
+  $(".xbtn").on("click", function(){
+    var text = $(this).text();
+    $("#x-axis").text(text);
   });
-
 });
+
+function myFunction(id) {
+    document.getElementById(id).classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
